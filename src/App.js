bd767582364424c5b137f392b5ce264f2e87cb3f
Xueting D <dxt_jenny@outlook.com>
2023-12-01 02:31:45 -0800

    add log in/out function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+// import WheelOfFortuneGame from './WheelOfFortuneGame';
+import LoginForm from './LoginForm';
+import HeaderComponent from './HeaderComponent';
 import WheelOfFortuneGame from './WheelOfFortuneGame';
+import { getAuth, signOut } from 'firebase/auth';
 
 function App() {
+  // user is the currently logged in user
+	const [user, setUser] = useState(null);
+
+  // this will be called by the LoginForm
+	function HandleLogin(user) {
+		setUser(user);
+	}
+
+  function HandleLogout() {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      console.log("User successfully logged out");
+      setUser(null);
+    }).catch((error) => {
+      console.error("Logout error", error);
+    });
+  }
+
   return (
     <div className="App">
-      <WheelOfFortuneGame />
+      {user ? (
+        <div>
+          <WheelOfFortuneGame/>
+          <div className='LogInOut'>
+            <button onClick={HandleLogout}>Log Out</button>
+          </div>
+        </div>
+       ) :(
+        <div>
+          <HeaderComponent/>
+          <LoginForm LoginEvent={HandleLogin}/>
+        </div>
+      )}
     </div>
   );
 }
