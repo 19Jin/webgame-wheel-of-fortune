24f1010da5b4cd689f827942fdfb85108db91293
dingjunyu888 <dingjunyu888@gmail.com>
2023-12-03 18:25:35 -0800

    Redeem Gift UI Complete

--- src/RedeemGift.js
+++ src/RedeemGift.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'; // Import Router and related components
-
+import react ,{useState, useEffect} from "react";
+import { Link } from 'react-router-dom';
+import './Gift.css';
+import axios from 'axios';
 
 
 function RedeemGift() {
 
+    const [gifts, setGifts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    function displayAllCommodity() {
+        axios.get('https://wheel-of-fortune-406910.wl.r.appspot.com/findAllCommodities')
+        .then(response => {
+          setGifts(response.data);  // Axios packs the response in a 'data' property
+          console.log(gifts);
+          setLoading(false);
+        })
+        .catch(error => {
+          setError(error.message);
+          setLoading(false);
+        });
     
-  // Add your code for the "Gift Redeem" page here
+    };
+  // Function to create rows with 3 gifts each
+  const createGiftRows = () => {
+    const rows = [];
+    for (let i = 0; i < gifts.length; i += 3) {
+      const rowGifts = gifts.slice(i, i + 3);
+      rows.push(
+        <tr key={i}>
+          {rowGifts.map(gift => (
+            <td key={gift.id}>
+              <button className="gift-button">
+                <img className="gift-image" src={gift.imageLink} alt={`Gift ${gift.number}`} />
+                <p>Gift Name: {gift.itemName}</p>
+                <p>Number: {gift.number}</p>
+                <p>Price: {gift.price}</p>
+              </button>
+            </td>
+          ))}
+        </tr>
+      );
+    }
+    return rows;
+  };
+
+
+  useEffect(() => {
+    // Using Axios to fetch data
+   
+        displayAllCommodity()
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+  
   return (
-    <div>
-      {/* Content for the "Gift Redeem" page */}
-      This is Gift Redeem
-      <Link to="/"><button>Back to game</button></Link>
+    <div className="redeem-gifts">
+      <h1 className="gift-header">Gift Redeem</h1>
+      <table className="gift-table">
+        <tbody>
+          {createGiftRows()}
+        </tbody>
+      </table>
+      <Link to="/"><button>Back to Game</button></Link>
     </div>
   );
 }
